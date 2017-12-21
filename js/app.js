6add37ebff1dd6bdb36968161ac9da89f745be64
michellesegv <michelle@laboratoria.la>
2017-12-21 08:39:31 -0500

    añadiendo práctica con selectores

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  // Añadiendo la clase 'purple' a los elementos 'li'
+  $('li').addClass('purple');
+  
+  // Añadiendo la clase 'yellow' al elemento con el id 'info'
+  $('#info').addClass('yellow');
+
+  // Añadiendo la clase 'red' a los elementos con la clase 'title'
+  $('.title').addClass('red');
+})
